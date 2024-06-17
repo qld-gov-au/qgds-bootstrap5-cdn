a216136bcd67ea338b90a0d0f13f6202509aab80
qld-gov-au-qgds-ServiceAccount <qld-gov-au-qgds-ServiceAccount@users.noreply.github.com>
2024-06-17 01:47:58 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:v1.0.16/dist REV:bd8fbf6331e9f655a1a9243966e9a341a772213b
    
    1.0.16

---
+++ v1/v1.0.16/assets/js/handlebars.helpers.js
@@ -0,0 +1,63 @@
+/* global Handlebars */
+
+/**
+ * Registers Handlebars Helpers
+ * @param {module} Handlebars Templating engine
+ * @returns {void} Result of the helper operation
+ */
+
+export default function handlebarsHelpers(Handlebars) {
+  // Contains - 
+  Handlebars.registerHelper("contains", function (needle, haystack, options) {
+    needle = Handlebars.escapeExpression(needle);
+    haystack = Handlebars.escapeExpression(haystack);
+    return haystack.indexOf(needle) > -1
+      ? options.fn(this)
+      : options.inverse(this);
+  });
+  // ifCond - checks conditions 
+  Handlebars.registerHelper("ifCond", function (v1, operator, v2, options) {
+    switch (operator) {
+    case "==":
+      return v1 == v2 ? options.fn(this) : options.inverse(this);
+    case "===":
+      return v1 === v2 ? options.fn(this) : options.inverse(this);
+    case "!=":
+      return v1 != v2 ? options.fn(this) : options.inverse(this);
+    case "!==":
+      return v1 !== v2 ? options.fn(this) : options.inverse(this);
+    case "<":
+      return v1 < v2 ? options.fn(this) : options.inverse(this);
+    case "<=":
+      return v1 <= v2 ? options.fn(this) : options.inverse(this);
+    case ">":
+      return v1 > v2 ? options.fn(this) : options.inverse(this);
+    case ">=":
+      return v1 >= v2 ? options.fn(this) : options.inverse(this);
+    case "&&":
+      return v1 && v2 ? options.fn(this) : options.inverse(this);
+    case "||":
+      return v1 || v2 ? options.fn(this) : options.inverse(this);
+    case "contains":
+      if (typeof v1 == "string" && typeof v2 == "string") {
+        return v1.toLowerCase().indexOf(v2.toLowerCase()) >= 0
+          ? options.fn(this)
+          : options.inverse(this);
+      } else return options.inverse(this);
+    default:
+      return options.inverse(this);
+    }
+  });
+  // Checks is expected type
+  Handlebars.registerHelper('isType', function (value, expected, options) {
+    if (value === expected) {
+      return options.fn(this); // Render the block if condition is true
+    } else {
+      return options.inverse(this); // Render the else block if present
+    }
+  });
+}
+
+if(typeof(Handlebars) !== 'undefined') {
+  handlebarsHelpers(Handlebars);
+}
