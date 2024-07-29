2b998defe8b3a07308ed4a24b6255a452572661e
qld-gov-au-qgds-ServiceAccount <qld-gov-au-qgds-ServiceAccount@users.noreply.github.com>
2024-07-29 01:49:58 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:v1.1.5/dist REV:37c0cbda4bf38cf7c0b2d3fc6011f8df6a23851a
    
    1.1.5

---
+++ v1/v1.1.5/assets/js/handlebars.helpers.js
@@ -0,0 +1,109 @@
+/* global Handlebars */
+/**
+ * Registers Handlebars Helpers
+ * @param {Handlebars} handlebars Templating engine
+ * @returns {void} Result of the helper operation
+ */
+
+export default function handlebarsHelpers(handlebars) {
+  // contains - if first object is in collection (second object) to return true
+  handlebars.registerHelper("contains", function (needle, haystack, options) {
+    needle = handlebars.escapeExpression(needle);
+    haystack = handlebars.escapeExpression(haystack);
+    return haystack.indexOf(needle) > -1
+      ? options.fn(this)
+      : options.inverse(this);
+  });
+  // ifCond - checks conditions
+  handlebars.registerHelper("ifCond", function (v1, operator, v2, options) {
+    switch (operator) {
+    case "==":
+      return v1 == v2 ? options.fn(this) : options.inverse(this);
+    case "===":
+      return v1 === v2 ? options.fn(this) : options.inverse(this);
+    case "!=":
+      return v1 != v2 ? options.fn(this) : options.inverse(this);
+    case "!==":
+      return v1 !== v2 ? options.fn(this) : options.inverse(this);
+    case "<":
+      return v1 < v2 ? options.fn(this) : options.inverse(this);
+    case "<=":
+      return v1 <= v2 ? options.fn(this) : options.inverse(this);
+    case ">":
+      return v1 > v2 ? options.fn(this) : options.inverse(this);
+    case ">=":
+      return v1 >= v2 ? options.fn(this) : options.inverse(this);
+    case "&&":
+      return v1 && v2 ? options.fn(this) : options.inverse(this);
+    case "||":
+      return v1 || v2 ? options.fn(this) : options.inverse(this);
+    case "contains":
+      if (typeof v1 == "string" && typeof v2 == "string") {
+        return v1.toLowerCase().indexOf(v2.toLowerCase()) >= 0
+          ? options.fn(this)
+          : options.inverse(this);
+      } else return options.inverse(this);
+    default:
+      return options.inverse(this);
+    }
+  });
+  // isType - Checks is expected type
+  handlebars.registerHelper('isType', function (value, expected, options) {
+    if (value === expected) {
+      return options.fn(this); // Render the block if condition is true
+    } else {
+      return options.inverse(this); // Render the else block if present
+    }
+  });
+  // ifAny - {{{#ifAny variable1 variable2 variable3 variable4 etc}}, if any set then return true
+  handlebars.registerHelper('ifAny', function (...args) {
+    const options = args.pop(); // The last argument is the options object
+    return args.some(arg => !!arg) ? options.fn(this) : options.inverse(this);
+  });
+  // now - return current timestamp i.e {{now}}
+  handlebars.registerHelper('now', function() {
+    return new Date().toISOString();
+  });
+  // formatDate - Format Date, for footer meta data i.e {{formatDate '2023-06-23'}}
+  handlebars.registerHelper('formatDate', function(dateString, defaultDate, format) {
+    // Use the dateString if provided, otherwise use the defaultDate, otherwise error
+    let date;
+    if (dateString) {
+      date = new Date(dateString);
+    }
+    if (isNaN(date) && defaultDate) {
+      date = new Date(defaultDate);
+    }
+
+    // Check if the date is valid
+    if (isNaN(date)) {
+      return 'Invalid Date';
+    }
+
+    var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var day = date.getDate();
+    var month = monthNames[date.getMonth()];
+    var year = date.getFullYear();
+    // Format date based on the format string
+    switch (format) {
+    case 'YYYY':
+      return `${year}`;
+    case 'MMMM YYYY':
+      return `${month} ${year}`;
+    default:
+      return `${day} ${month} ${year}`;
+    }
+  });
+
+  // formatDateOrToday - Format Date if passed or today i.e. {{formatDateOrToday '2023-06-23'}} or {{formatDateOrToday}} <!-- This will use the current date -->
+  handlebars.registerHelper('formatDateOrToday', function(dateString, format) {
+    // Use the current date if dateString is missing or invalid
+    const dateToFormat = dateString || new Date().toISOString();
+    // Call the formatDate helper with the determined date and format
+    return handlebars.helpers.formatDate(dateString, dateToFormat, format);
+  });
+}
+
+if(typeof(Handlebars) !== 'undefined') {
+  handlebarsHelpers(Handlebars);
+}
